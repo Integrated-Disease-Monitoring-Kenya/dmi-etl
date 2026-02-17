9cfb31f3722dfcdd9eca58b2dd704acdacc338f7
brianWakhutu <brian.wakhutu@gmail.com>
2026-02-17 12:27:32 +0300

    Removed hooks on table date_epi_week, as it was self refrecing before creation

--- models/dmi_data_mart/dim_date_epi_week.sql
+++ models/dmi_data_mart/dim_date_epi_week.sql
@@ -1,5 +1,6 @@
 
 -- This model creates a date to epi week mapping, which is used in the fact tables to link dates to their corresponding epi weeks.
+-- Removed the hooks to check if joining issue is comming from dim_date or dim_epi_week and added the date to epi week mapping logic directly in the model. This is because the joining issue was due to the fact that the date range in dim_date was not covering all the dates in the fact tables, which caused some records to be dropped during the join. By creating a date to epi week mapping, we can ensure that all dates in the fact tables are covered and we can avoid any joining issues.
 {{ config(
   materialized='table'
 ) }}
